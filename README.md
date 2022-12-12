1681bfe7a1a60dc3afd212020eaf8116d27db065
Noah Braasch <braasch.noah@gmail.com>
2022-12-11 19:15:21 -0500

    some tweaks, and spices up readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Anki-CSV-To-Cards
-This is a program I am working on to convers a list of the 10k most frequent words found on Japanese netflix, into Anki cards I can study. Im not very experienced with python so this has been an interesting challenge
+
+This is a program I wrote to convert a list of the 12k most frequent words found on Japanese netflix, into Anki cards that I can study. Im not very experienced with python so this has been an interesting challenge.
+
+## Running from VS Code
+![[ezgif.com-gif-maker(2).gif]]
+When running, this code simulate keyboard inputs by the `pyautogui` library and manually fills the fields of the flashcards
+![[2022-12-11 17-24-59.gif]]
+There is certainly a faster way to do this. But I have only written very limited amounts of python in the past so this was sufficient for myself.
--- src/create_cards.py
+++ src/create_cards.py
@@ -4,6 +4,8 @@ import time
 import pyautogui
 import keyboard
 from threading import Thread
+import os
+import pyperclip
 
 global terminate # Global flag for kill switching
 terminate = False
@@ -11,16 +13,25 @@ terminate = False
 def check_for_kill(): # Kill switch
 	global terminate 
 	while True:
-		if keyboard.read_key() == 'k':
+		if keyboard.read_key() == 'esc':
 			terminate = True
 			break
+
+def paste():	# Paste into field
+	with pyautogui.hold('ctrl'):
+		pyautogui.press('v')
 
 def main():
 	string = '' # words from csv to be added to list
 	word_list = [] # list of words from csv
-	running_total = 1
+	running_total = 0
 
-	with open('../files/Netflix_95%_12K.csv', 'r', encoding='UTF-8') as csv_file: # Parses csv word list into dictionary
+
+	absolute_path = os.path.dirname(__file__)
+	relative_path = "../files/Netflix_95%_12K.csv"
+	full_path = os.path.join(absolute_path, relative_path) #filepath to csv
+
+	with open(full_path, 'r', encoding='UTF-8') as csv_file: # Parses csv word list into dictionary
 		csv_string = csv_file.read()
 		for member in csv_string:
 			if member != '\n':
@@ -44,27 +55,49 @@ def main():
 			print('Probably got another 429')
 			time.sleep(5)
 
+		# Reads through the JSON data and steps down through each field, adding some formatting and then entering the card
+
 		try:
 			json_index_1= 0
 			json_index_2= 0
 			print(running_total, end=' ')
 			print(json_dict['meta']['status'], end=' ')
+
+			pyperclip.copy(json_dict['data'][json_index_1]['slug'])
+			paste()
+			pyautogui.press('tab')
 			print(json_dict['data'][json_index_1]['slug'], end=' ')
+
+			pyperclip.copy(json_dict['data'][json_index_1]['japanese'][json_index_2]['reading'])
+			paste()
+			pyautogui.press('tab')
 			print(json_dict['data'][json_index_1]['japanese'][json_index_2]['reading'], end=' ')
+
 			for definiton in json_dict:	
 				try:
 					for definiton in json_dict['data'][json_index_1]['senses'][json_index_2]['english_definitions']:
 						if terminate == True:
 							break
+						pyperclip.copy(json_dict['data'][json_index_1]['senses'][json_index_2]['english_definitions'][json_index_2])
+						paste()
+						pyautogui.write(': ')
 						print(json_dict['data'][json_index_1]['senses'][json_index_2]['english_definitions'][json_index_2], end=' ')
+
+						pyperclip.copy(json_dict['data'][json_index_1]['senses'][json_index_2]['parts_of_speech'][json_index_2])
+						paste()
+						pyautogui.write('; ')
 						print(json_dict['data'][json_index_1]['senses'][json_index_2]['parts_of_speech'][json_index_2], end=' ')
 						json_index_2+=1
+					pyautogui.press('tab')
 				except:
 					json_index_2 = 0
 					json_index_1 += 1
 				if terminate:
 					break
 			print('ADDED', end='')
+			pyautogui.press('a')
+			with pyautogui.hold('ctrl'):
+				pyautogui.press('enter')
 		except:
 			json_index_1 = 0
 		print()
